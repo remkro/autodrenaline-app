c420f3b7dae5c8cfb2cc4f6a9cd05a2ffdc6268e
Remigiusz Królak <krolak.remek@gmail>
2022-11-10 12:40:12 +0100

    Added Car entity and update pom.xml file with Spring Data JPA

---
+++ src/main/java/com/autodrenaline/autodrenalineapp/entity/Car.java
@@ -0,0 +1,68 @@
+package com.autodrenaline.autodrenalineapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String model;
+    private String brand;
+    private short year;
+    private int mileage;
+
+    public Car() {
+    }
+
+    public Car(String model, String brand, short year, int mileage) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+}
