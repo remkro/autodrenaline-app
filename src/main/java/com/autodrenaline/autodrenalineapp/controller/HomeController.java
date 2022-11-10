66b018c92924897a705ecd1394265136d6876215
Remigiusz Królak <krolak.remek@gmail>
2022-11-10 12:26:58 +0100

    Added HomeController class to handle homepage view

---
+++ src/main/java/com/autodrenaline/autodrenalineapp/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.autodrenaline.autodrenalineapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class HomeController {
+
+    @GetMapping
+    public String getHome(Model model) {
+        return "index";
+    }
+
+}
