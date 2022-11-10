f54021b0e92b0294c2091c53a6adc6538daaef0d
Remigiusz Królak <krolak.remek@gmail>
2022-11-10 12:12:10 +0100

    Created project and added dependencies

---
+++ src/main/java/com/autodrenaline/autodrenalineapp/AutodrenalineAppApplication.java
@@ -0,0 +1,13 @@
+package com.autodrenaline.autodrenalineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutodrenalineAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutodrenalineAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autodrenaline/autodrenalineapp/AutodrenalineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autodrenaline.autodrenalineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodrenalineAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
