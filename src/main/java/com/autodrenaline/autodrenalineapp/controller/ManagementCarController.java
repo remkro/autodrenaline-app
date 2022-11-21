239903285eb51f5f28a82fc6121b428c18f02739
Remigiusz Królak <krolak.remek@gmail.com>
2022-11-21 20:51:36 +0100

    Added mapping for cars list (temporary using in-memory list)

--- src/main/java/com/autodrenaline/autodrenalineapp/controller/ManagementCarController.java
+++ src/main/java/com/autodrenaline/autodrenalineapp/controller/ManagementCarController.java
@@ -6,9 +6,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/management/cars")
 public class ManagementCarController {
+
+    private List<Car> cars = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        cars.add(new Car("Ferrari", "458 Italia", (short) 2010, 50_000));
+    }
 
     @GetMapping("/add")
     public String getCarAddForm(Model model) {
@@ -16,4 +27,11 @@ public class ManagementCarController {
         model.addAttribute("car", car);
         return "add-car-form";
     }
+
+    @GetMapping("/list")
+    public String getCarsList(Model model) {
+        model.addAttribute("cars", cars);
+        return "cars-list";
+    }
+
 }
