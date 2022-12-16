9e9d78d624664922655b49abcecf7b864690f6d6
Remigiusz Królak <krolak.remek@gmail.com>
2022-12-16 22:19:23 +0100

    Added rate fild in Car entity, added Lombok annotations

--- src/main/java/com/autodrenaline/autodrenalineapp/entity/Car.java
+++ src/main/java/com/autodrenaline/autodrenalineapp/entity/Car.java
@@ -1,89 +1,26 @@
 package com.autodrenaline.autodrenalineapp.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Car {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String brand;
     private String model;
+    private BigDecimal rate;
     private short horsePower;
     private short yearOfProduction;
     private int mileage;
     boolean currentlyAvailable;
-
-    public Car() {
-    }
-
-    public Car(String brand, String model, short horsePower, short yearOfProduction, int mileage, boolean currentlyAvailable) {
-        this.brand = brand;
-        this.model = model;
-        this.horsePower = horsePower;
-        this.yearOfProduction = yearOfProduction;
-        this.mileage = mileage;
-        this.currentlyAvailable = currentlyAvailable;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public short getHorsePower() {
-        return horsePower;
-    }
-
-    public void setHorsePower(short horsePower) {
-        this.horsePower = horsePower;
-    }
-
-    public short getYearOfProduction() {
-        return yearOfProduction;
-    }
-
-    public void setYearOfProduction(short yearOfProduction) {
-        this.yearOfProduction = yearOfProduction;
-    }
-
-    public int getMileage() {
-        return mileage;
-    }
-
-    public void setMileage(int mileage) {
-        this.mileage = mileage;
-    }
-
-    public boolean isCurrentlyAvailable() {
-        return currentlyAvailable;
-    }
-
-    public void setCurrentlyAvailable(boolean currentlyAvailable) {
-        this.currentlyAvailable = currentlyAvailable;
-    }
-
 }
