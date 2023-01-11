6b929226362b3fb46c1e8b75906656a973ec007e
Remigiusz Królak <krolak.remek@gmail.com>
2023-01-11 20:26:57 +0100

    Implemented service responsible for discount related tasks

--- src/main/java/com/autodrenaline/autodrenalineapp/repository/ClientRepository.java
+++ src/main/java/com/autodrenaline/autodrenalineapp/repository/ClientRepository.java
@@ -3,6 +3,8 @@ package com.autodrenaline.autodrenalineapp.repository;
 import com.autodrenaline.autodrenalineapp.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Client findByUsername(String username);
+    Optional<Client> findByUsername(String username);
 }
--- src/main/java/com/autodrenaline/autodrenalineapp/repository/RentEventRepository.java
+++ src/main/java/com/autodrenaline/autodrenalineapp/repository/RentEventRepository.java
@@ -2,6 +2,9 @@ package com.autodrenaline.autodrenalineapp.repository;
 
 import com.autodrenaline.autodrenalineapp.entity.RentEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RentEventRepository extends JpaRepository<RentEvent, Long> {
+    @Query("SELECT SUM(r.duration) FROM RentEvent r WHERE r.client.id = ?1")
+    int countTotalRentDurationCompletedByCustomer(long id);
 }
